785fed9f5ca51dc5d9f2cd65dbc9355f7308e3ad
Maksim Kryshneu <maks.kryshnev@gmail.com>
2019-10-04 15:45:33 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stuck = [];
+    let array = str.split("");
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (array[i] === bracketsConfig[j][0]
+                && array[i] !== bracketsConfig[j][1]) {
+                stuck.push(array[i])
+            } else if (array[i] === bracketsConfig[j][0] &&
+                array[i] === bracketsConfig[j][1] &&
+                stuck[stuck.length - 1] !== array[i]) {
+                stuck.push(array[i])
+            } else if (array[i] === bracketsConfig[j][0] &&
+                array[i] === bracketsConfig[j][1] &&
+                stuck[stuck.length - 1] === array[i]
+                && stuck.length !== 0) {
+                stuck.pop()
+            } else if (array[i] !== bracketsConfig[j][0] &&
+                array[i] === bracketsConfig[j][1] &&
+                stuck[stuck.length - 1] === bracketsConfig[j][0]
+                && stuck.length !== 0) {
+                stuck.pop();
+            } else if (array[i] !== bracketsConfig[j][0] &&
+                array[i] === bracketsConfig[j][1]
+                && stuck.length === 0) {
+                stuck.push("error")
+            }
+        }
+    }
+    return (stuck.length === 0)
 }
